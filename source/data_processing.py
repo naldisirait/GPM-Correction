c3cebc67ba7a859f8dce79fab07eb4f005485f5f
naldisirait <23522052@std.stei.itb.ac.id>
2024-02-18 10:10:13 +0700

    update

--- source/data_processing.py
+++ source/data_processing.py
@@ -19,3 +19,21 @@ def process_data(df: pd.DataFrame, config):
     
     return X,y
 
+
+
+def melt_df(df, id_vars, var_name, value_name):
+    """
+    Function to melted dataframe
+    Args:
+        df: dataset in form of a dataframe
+        id_vars: retained columns
+        var_name: column name for all the melted columns
+        value_name: column name for all the melted values
+
+    Returns:
+        df_melted: df melted
+    
+    """
+    df_melted = pd.melt(df, id_vars=id_vars, var_name=var_name, value_name=value_name)
+
+    return df_melted
---
+++ source/model.py
@@ -0,0 +1,6 @@
+import torch
+
+def create_model(model_name):
+    if model_name == "linear regression":
+        model = None
+    return model
